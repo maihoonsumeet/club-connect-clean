921db5df0def36449391a13f05cfafda3602b075
Sumeet Sharma <176627124+maihoonsumeet@users.noreply.github.com.>
2025-07-20 21:03:47 +0530

    new supabseclient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 // IMPORTANT: Replace with your own Supabase project URL and anon key
-const supabaseUrl = 'https://adlsogvfwodfjoxtothk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkbHNvZ3Zmd29kZmpveHRvdGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDc0OTEsImV4cCI6MjA2ODA4MzQ5MX0.4IksHxCoEG_SLENrkXja6C9eEjD6YD7ETq5b5sln-XQ';
+const supabaseUrl = import.meta.env.'https://adlsogvfwodfjoxtothk.supabase.co';
+const supabaseAnonKey = import.meta.env.'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkbHNvZ3Zmd29kZmpveHRvdGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDc0OTEsImV4cCI6MjA2ODA4MzQ5MX0.4IksHxCoEG_SLENrkXja6C9eEjD6YD7ETq5b5sln-XQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
 if (!supabaseUrl || supabaseUrl.includes('https://adlsogvfwodfjoxtothk.supabase.co')) {
